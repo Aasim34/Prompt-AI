9451e55d56b461b9fb18c5fd9e3986317a2a1bb2
Mohammad Vasim <mohammadvasim353@gmail.com>
2025-11-05 11:22:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5914096800-6c563",
+  "appId": "1:229430277259:web:b0c8d5e37e784ae7d7908e",
+  "apiKey": "AIzaSyAl5ZGMP5D13_A92HF6ODvJJ3wN0SzI6DY",
+  "authDomain": "studio-5914096800-6c563.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "229430277259"
+};
